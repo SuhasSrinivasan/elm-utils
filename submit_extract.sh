d56ea0ccefcb763f11b775200022031465373008
SuhasSrinivasan <32346517+SuhasSrinivasan@users.noreply.github.com>
2025-06-17 14:31:25 -0700

    Update submit_extract.sh

--- submit_extract.sh
+++ submit_extract.sh
@@ -3,20 +3,23 @@
 # This wraps extract_tar.sbatch
 
 # === Usage check ===
-if [ $# -ne 3 ]; then
-  echo "Usage: $0 <username> <tool:dtar/tar> <time>"
-  echo "Example: $0 user dtar 2-00:00:00"
+if [ $# -ne 4 ]; then
+  echo "Usage: $0 <username> <tool:dtar/tar> <ntasks> <time>"
+  echo "Example 1: $0 user dtar 8 1-00"
+  echo "Example 2: $0 user tar 1 2-00"
   exit 1
 fi
 
 USERNAME="$1"
 TOOL="$2"
-TIME="$3"
+NTASKS="$3"
+TIME="$4"
 
 # === Submit the job ===
 sbatch \
   --job-name=extract_${USERNAME} \
-  -o extract_${USERNAME}.out \
-  -e extract_${USERNAME}.err \
+  --output extract_${USERNAME}.out \
+  --error extract_${USERNAME}.err \
+  --ntasks ${NTASKS} \
   --time=${TIME} \
-  extract_tar.sbatch ${USERNAME} ${TOOL}
+  extract_tar.sbatch ${USERNAME} ${TOOL} ${NTASKS}
